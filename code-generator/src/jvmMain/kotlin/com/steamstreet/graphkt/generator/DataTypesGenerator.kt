9b8fbdd814aef2ad860a24ef58dba19a9b45c3f1
Jon Nichols <jon.nichols@siriusxm.com>
2025-05-25 18:24:26 -0700

    Change from enum serialization to sealed classes that allow unknown values.

--- code-generator/src/jvmMain/kotlin/com/steamstreet/graphkt/generator/DataTypesGenerator.kt
+++ code-generator/src/jvmMain/kotlin/com/steamstreet/graphkt/generator/DataTypesGenerator.kt
@@ -1,10 +1,17 @@
 package com.steamstreet.graphkt.generator
 
 import com.squareup.kotlinpoet.*
+import com.squareup.kotlinpoet.ParameterizedTypeName.Companion.parameterizedBy
 import graphql.language.EnumTypeDefinition
 import graphql.language.InputObjectTypeDefinition
 import graphql.schema.idl.ScalarInfo
 import graphql.schema.idl.TypeDefinitionRegistry
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
 import java.io.File
 import java.util.*
 
@@ -49,31 +56,42 @@ class DataTypesGenerator(
         val serializers = schema.customScalars().filter { scalarSerializer(it.name) != null }
 
         if (serializers.isNotEmpty()) {
-            commonFile.addProperty(PropertySpec.builder("serializerModule",
-                    ClassName("kotlinx.serialization.modules", "SerializersModule"))
+            commonFile.addProperty(
+                PropertySpec.builder(
+                    "serializerModule",
+                    ClassName("kotlinx.serialization.modules", "SerializersModule")
+                )
                     .initializer(CodeBlock.builder().apply {
-                        this.beginControlFlow("%T",
-                                ClassName("kotlinx.serialization.modules", "SerializersModule"))
+                        this.beginControlFlow(
+                            "%T",
+                            ClassName("kotlinx.serialization.modules", "SerializersModule")
+                        )
 
                         serializers.forEach { scalar ->
                             val scalarClass = scalarClass(scalar.name)
                             val scalarSerializer = scalarSerializer(scalar.name)
 
                             if (scalarSerializer != null) {
-                                addStatement("contextual(%T::class, %T)",
-                                        scalarClass, scalarSerializer)
+                                addStatement(
+                                    "contextual(%T::class, %T)",
+                                    scalarClass, scalarSerializer
+                                )
                             }
                         }
 
                         this.endControlFlow()
                     }.build())
-                    .build())
+                    .build()
+            )
 
         }
 
         val jsonSerializerType = ClassName("kotlinx.serialization.json", "Json")
-        commonFile.addProperty(PropertySpec.builder("json",
-                jsonSerializerType)
+        commonFile.addProperty(
+            PropertySpec.builder(
+                "json",
+                jsonSerializerType
+            )
                 .addModifiers(KModifier.INTERNAL)
                 .initializer(CodeBlock.builder().apply {
                     beginControlFlow("%T", jsonSerializerType)
@@ -83,7 +101,8 @@ class DataTypesGenerator(
                     addStatement("ignoreUnknownKeys = true")
                     endControlFlow()
                 }.build())
-                .build())
+                .build()
+        )
     }
 
     fun generateInputTypes() {
@@ -100,47 +119,330 @@ class DataTypesGenerator(
                         val typeName = getKotlinType(inputValue.type)
 
                         addParameter(
-                                ParameterSpec.builder(inputValue.name,
-                                        typeName).apply {
-                                    if (typeName.isNullable) {
-                                        defaultValue("null")
-                                    }
+                            ParameterSpec.builder(
+                                inputValue.name,
+                                typeName
+                            ).apply {
+                                if (typeName.isNullable) {
+                                    defaultValue("null")
+                                }
 
-                                    schema.findScalar(inputValue.type)?.let {
-                                        properties["scalar.${it.name}.serializer"]
-                                    }?.let {
-                                        addAnnotation(ClassName("kotlinx.serialization", "Contextual"))
-                                    }
-//                                    schema.buildSerializableAnnotation(inputValue.type)?.let {
-//                                        addAnnotation(it)
-//                                    }
-                                }.build()
+                                schema.findScalar(inputValue.type)?.let {
+                                    properties["scalar.${it.name}.serializer"]
+                                }?.let {
+                                    addAnnotation(ClassName("kotlinx.serialization", "Contextual"))
+                                }
+                            }.build()
                         )
                     }
                 }.build())
 
                 inputType.inputValueDefinitions.forEach { inputValue ->
-                    addProperty(PropertySpec.builder(inputValue.name,
-                            getKotlinType(inputValue.type))
-                            .initializer(inputValue.name).build())
+                    addProperty(
+                        PropertySpec.builder(
+                            inputValue.name,
+                            getKotlinType(inputValue.type)
+                        )
+                            .initializer(inputValue.name).build()
+                    )
                 }
             }
             commonFile.addType(inputTypeClass.build())
         }
 
         schema.types().values.mapNotNull { it as? EnumTypeDefinition }.forEach { enumType ->
-            commonFile.addType(TypeSpec.enumBuilder(ClassName(packageName, enumType.name)).apply {
-                enumType.comments?.forEach {
-                    this.addKdoc(it.content)
+//            val enumClassName = ClassName(packageName, enumType.name)
+            val enumSealedClassName = ClassName(packageName, enumType.name)
+            val serializerClassName = ClassName(
+                enumSealedClassName.packageName, enumSealedClassName.simpleName + "Serializer"
+            )
+
+            val enumSerializer = TypeSpec.objectBuilder(
+                serializerClassName
+            )
+            enumSerializer.addSuperinterface(
+                KSerializer::class.asClassName()
+                    .parameterizedBy(enumSealedClassName)
+            )
+
+            enumSerializer.addProperty(
+                PropertySpec.builder(
+                    "descriptor",
+                    SerialDescriptor::class, KModifier.OVERRIDE
+                )
+                    .initializer(
+                        "%M(%S, %T.STRING)",
+                        MemberName("kotlinx.serialization.descriptors", "PrimitiveSerialDescriptor"),
+                        enumSealedClassName.simpleName,
+                        PrimitiveKind::class
+                    )
+                    .build()
+            )
+
+
+
+            enumSerializer.addFunction(
+                FunSpec.builder("serialize")
+                    .addModifiers(KModifier.OVERRIDE)
+                    .addParameter("encoder", Encoder::class)
+                    .addParameter("value", enumSealedClassName)
+                    .addStatement("encoder.encodeString(value.name)")
+                    .build()
+            )
+
+            enumSerializer.addFunction(
+                FunSpec.builder("deserialize")
+                    .addModifiers(KModifier.OVERRIDE)
+                    .returns(enumSealedClassName)
+                    .addParameter("decoder", Decoder::class)
+                    .addCode(
+                        CodeBlock.builder()
+                            .addStatement("return %T.valueOf(decoder.decodeString())", enumSealedClassName)
+                            .build()
+                    )
+                    .build()
+            )
+
+            commonFile.addType(enumSerializer.build())
+
+            commonFile.addType(TypeSpec.classBuilder(enumSealedClassName).apply {
+                primaryConstructor(
+                    FunSpec.constructorBuilder()
+                        .addParameter(ParameterSpec.builder("name", String::class).build())
+                        .build()
+                )
+                addProperty(
+                    PropertySpec.builder("name", String::class)
+                        .initializer("name").build()
+                )
+                addModifiers(KModifier.SEALED)
+                addAnnotation(
+                    AnnotationSpec.builder(Serializable::class)
+                        .addMember("with = %T::class", serializerClassName)
+                        .build()
+                )
+
+                addFunction(
+                    FunSpec.builder("toString").addModifiers(KModifier.OVERRIDE)
+                        .returns(String::class)
+                        .addStatement("return name")
+                        .build()
+                )
+
+                enumType.enumValueDefinitions.forEach { enumValue ->
+                    addType(
+                        TypeSpec.objectBuilder(enumValue.name)
+                            .superclass(enumSealedClassName)
+                            .addSuperclassConstructorParameter(
+                                "%S", enumValue.name
+                            )
+                            .build()
+                    )
                 }
-                enumType.enumValueDefinitions.forEach {
-                    this.addEnumConstant(it.name, TypeSpec.anonymousClassBuilder().apply {
-                        it.comments?.forEach {
-                            this.addKdoc(it.content)
-                        }
-                    }.build())
-                }
+
+                addType(
+                    TypeSpec.classBuilder("Unknown")
+                        .superclass(enumSealedClassName)
+                        .primaryConstructor(
+                            FunSpec.constructorBuilder()
+                                .addParameter(ParameterSpec.builder("name", String::class).build())
+                                .build()
+                        )
+                        .addSuperclassConstructorParameter(
+                            "name"
+                        )
+                        .build()
+                )
+
+                addType(
+                    TypeSpec.companionObjectBuilder().addFunction(
+                        FunSpec.builder("valueOf")
+                            .addParameter("name", String::class)
+                            .returns(enumSealedClassName)
+                            .addCode(
+                                CodeBlock.builder()
+                                    .beginControlFlow("return when (name)")
+                                    .apply {
+                                        enumType.enumValueDefinitions.forEach { enumValue ->
+                                            addStatement("%S -> %L", enumValue.name, enumValue.name)
+                                        }
+                                        addStatement("else -> Unknown(name)")
+                                    }
+                                    .endControlFlow()
+                                    .build()
+                            )
+                            .build()
+                    ).build()
+                )
             }.build())
+//
+//            commonFile.addType(TypeSpec.enumBuilder(enumClassName).apply {
+//                enumType.comments?.forEach {
+//                    this.addKdoc(it.content)
+//                }
+//                enumType.enumValueDefinitions.forEach {
+//                    this.addEnumConstant(it.name, TypeSpec.anonymousClassBuilder().apply {
+//                        it.comments?.forEach {
+//                            this.addKdoc(it.content)
+//                        }
+//                    }.build())
+//                }
+//            }.build())
         }
     }
+//
+//    private fun generateEnum(definition: EnumTypeDefinition) {
+//        val name = ClassName(id.substringBeforeLast("."), id.substringAfterLast("."))
+//
+//        val serializerClassName = ClassName(
+//            name.packageName, name.simpleName + "Serializer"
+//        )
+//        val enumSerializer = TypeSpec.objectBuilder(
+//            serializerClassName
+//        )
+//        enumSerializer.addSuperinterface(
+//            KSerializer::class.asClassName()
+//                .parameterizedBy(name)
+//        )
+//
+//        enumSerializer.addProperty(
+//            PropertySpec.builder(
+//                "descriptor",
+//                SerialDescriptor::class, KModifier.OVERRIDE
+//            )
+//                .initializer(
+//                    "%M(%S, %T.STRING)",
+//                    MemberName("kotlinx.serialization.descriptors", "PrimitiveSerialDescriptor"),
+//                    name.simpleName,
+//                    PrimitiveKind::class
+//                )
+//                .build()
+//        )
+//
+//
+//        var enumType = TypeSpec.classBuilder(name).apply {
+//            primaryConstructor(
+//                FunSpec.constructorBuilder()
+//                    .addParameter(ParameterSpec.builder("name", String::class).build())
+//                    .build()
+//            )
+//            addProperty(
+//                PropertySpec.builder("name", String::class)
+//                    .initializer("name").build()
+//            )
+//            addModifiers(KModifier.SEALED)
+//            addAnnotation(
+//                AnnotationSpec.builder(Serializable::class)
+//                    .addMember("with = %T::class", serializerClassName)
+//                    .build()
+//            )
+//
+//            addFunction(
+//                FunSpec.builder("toString").addModifiers(KModifier.OVERRIDE)
+//                    .returns(String::class)
+//                    .addStatement("return name")
+//                    .build()
+//            )
+//        }.build()
+//
+//        fun getEnumMembers(): List<TypeSpec> {
+//            return definition.enumValueDefinitions.mapNotNull { valueDefinition ->
+//
+//                val enumValue = member.traits["smithy.api#enumValue"]?.jsonPrimitive?.contentOrNull
+//                if (enumValue != null) {
+//                    TypeSpec.objectBuilder(memberId)
+//                        .superclass(name)
+//                        .addSuperclassConstructorParameter(
+//                            "%S", enumValue
+//                        )
+//                        .addTraitDocs(member.traits)
+//                        .build()
+//                } else null
+//            }
+//        }
+//
+//        enumType = enumType.toBuilder().apply {
+//            shape.mixins.forEach { mixin ->
+//                val mixinShape = spec.shapes[mixin.target]
+//                if (mixinShape != null) {
+//                    getEnumMembers(mixinShape).forEach {
+//                        this.addType(it)
+//                    }
+//                }
+//            }
+//
+//            getEnumMembers(shape).forEach {
+//                this.addType(it)
+//            }
+//
+//            addType(
+//                TypeSpec.classBuilder("Unknown")
+//                    .superclass(name)
+//                    .primaryConstructor(
+//                        FunSpec.constructorBuilder()
+//                            .addParameter(ParameterSpec.builder("name", String::class).build())
+//                            .build()
+//                    )
+//                    .addSuperclassConstructorParameter(
+//                        "name"
+//                    )
+//                    .build()
+//            )
+//
+//            addType(
+//                TypeSpec.companionObjectBuilder().addFunction(
+//                    FunSpec.builder("fromName")
+//                        .addParameter("name", String::class)
+//                        .returns(name)
+//                        .addCode(
+//                            CodeBlock.builder()
+//                                .beginControlFlow("return when (name)")
+//                                .apply {
+//                                    fun addShapeMembers(shape: Shape) {
+//                                        shape.members.forEach { (memberName, member) ->
+//                                            val enumValue =
+//                                                member.traits.smithyTrait("enumValue")?.jsonPrimitive?.contentOrNull
+//                                            addStatement("%S -> %L", enumValue, memberName)
+//                                        }
+//                                    }
+//
+//                                    shape.mixins.forEach { mixin ->
+//                                        spec.shapes[mixin.target]?.let {
+//                                            addShapeMembers(it)
+//                                        }
+//                                    }
+//                                    addShapeMembers(shape)
+//                                    addStatement("else -> Unknown(name)")
+//                                }
+//                                .endControlFlow()
+//                                .build()
+//                        )
+//                        .build()
+//                ).build()
+//            )
+//        }.build()
+//
+//        // now generate the serialization calls
+//        enumSerializer.addFunction(
+//            FunSpec.builder("serialize")
+//                .addModifiers(KModifier.OVERRIDE)
+//                .addParameter("encoder", Encoder::class)
+//                .addParameter("value", name)
+//                .addStatement("encoder.encodeString(value.name)")
+//                .build()
+//        )
+//
+//        enumSerializer.addFunction(
+//            FunSpec.builder("deserialize")
+//                .addModifiers(KModifier.OVERRIDE)
+//                .returns(name)
+//                .addParameter("decoder", Decoder::class)
+//                .addCode(
+//                    CodeBlock.builder()
+//                        .addStatement("return %T.fromName(decoder.decodeString())", name)
+//                        .build()
+//                )
+//                .build()
+//        )
+//    }
 }
